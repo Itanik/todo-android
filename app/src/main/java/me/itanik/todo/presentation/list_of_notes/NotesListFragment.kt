93e03ff8912d5ff094bd3cf3b1122cfbe2274ade
Nikita Ulyantsev <31597703+Itanik@users.noreply.github.com>
2022-07-20 03:06:08 +0100

    Add swipe to update behavior. (#8)

--- app/src/main/java/me/itanik/todo/presentation/list_of_notes/NotesListFragment.kt
+++ app/src/main/java/me/itanik/todo/presentation/list_of_notes/NotesListFragment.kt
@@ -3,12 +3,17 @@ package me.itanik.todo.presentation.list_of_notes
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import me.itanik.todo.R
 import me.itanik.todo.common.ItemSwipeCallback
+import me.itanik.todo.data.model.Result
 import me.itanik.todo.databinding.FragmentNotesListBinding
 import me.itanik.todo.presentation.base.BaseFragment
 import me.itanik.todo.presentation.note.NOTE_ID_ARG_TAG
@@ -22,6 +27,15 @@ class NotesListFragment : BaseFragment<FragmentNotesListBinding>() {
     private val viewModel by viewModel(NoteListViewModel::class.java) { noteListViewModelFactory() }
     override val bindingInflater: (LayoutInflater) -> FragmentNotesListBinding
         get() = FragmentNotesListBinding::inflate
+    private var observingStateJob: Job? = null
+    private val noteListAdapter by lazy {
+        NoteListAdapter { clickedNote ->
+            val args = Bundle().apply {
+                putString(NOTE_ID_ARG_TAG, clickedNote.id.toString())
+            }
+            findNavController().navigate(R.id.action_NotesListFragment_to_NoteFragment, args)
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,31 +45,42 @@ class NotesListFragment : BaseFragment<FragmentNotesListBinding>() {
         }
 
         initRecyclerView()
+        initDataObserving()
+
+        binding.swipeRefresh.setOnRefreshListener {
+            viewModel.updateNotesList()
+        }
     }
 
     private fun initRecyclerView() {
-        val adapter = NoteListAdapter { clickedNote ->
-            val args = Bundle().apply {
-                putString(NOTE_ID_ARG_TAG, clickedNote.id.toString())
-            }
-            findNavController().navigate(R.id.action_NotesListFragment_to_NoteFragment, args)
-        }
-
         // item deleting
-        binding.recyclerView.adapter = adapter
+        binding.recyclerView.adapter = noteListAdapter
         val onItemSwiped = ItemSwipeCallback { position ->
-            val note = adapter.currentList[position]
+            val note = noteListAdapter.currentList[position]
             viewModel.removeNote(note)
         }
         ItemTouchHelper(onItemSwiped).attachToRecyclerView(binding.recyclerView)
+    }
 
-        // item list updating
-        lifecycleScope.launchWhenStarted {
-            viewModel.notesStateFlow.collectLatest { noteList ->
-                Timber.d("Submit notes list")
-                adapter.submitList(noteList)
-            }
+    private fun initDataObserving() {
+        observingStateJob = lifecycleScope.launch {
+            viewModel.noteListStateFlow
+                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .collectLatest { result ->
+                    when (result) {
+                        is Result.Success -> {
+                            binding.swipeRefresh.isRefreshing = false
+                            noteListAdapter.submitList(result.data.noteList)
+                            Timber.d("Submit notes list")
+                        }
+                    }
+                }
         }
         viewModel.updateNotesList()
+    }
+
+    override fun onStop() {
+        observingStateJob?.cancel()
+        super.onStop()
     }
 }
